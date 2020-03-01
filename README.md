333a8f6fb2b9a48b2d7e04e4b2453f225d8c0725
chenjing <j-chen16@mails.tsinghua.edu.cn>
2020-03-01 18:03:43 +0800

    update codes

--- README.md
+++ README.md
@@ -6,4 +6,18 @@
 - CPU or NVIDIA GPU + CUDA CuDNN
 
 ## Getting started
-### 
+### train altitude-based RL model
+
+Train with the command line:
+```
+python agent_cap.py --topology ...
+```
+
+See more training options with `python agent_cap.py --help`
+
+### train Learning-To-Route model
+
+Train with the command line:
+```
+python agent.py --topology ...
+```
--- gnnpolicy.py
+++ gnnpolicy.py
@@ -12,16 +12,61 @@ class MlpPolicy_3l(FeedForwardPolicy):
 
 
 class GnnPolicy(FeedForwardPolicy):
-    def __init__(self,sess,ob_space,ac_space,n_env,n_steps,n_batch,reuse=False,**kwargs):
+    def __init__(self,sess,ob_space,ac_space,n_env,n_steps,n_batch,input_dim, hid_dims, out_dim, reuse=False,**kwargs):
         super(GnnPolicy, self).__init__(sess,ob_space,ac_space,n_env,n_steps,n_batch,reuse=reuse,scale=True)
 
         with tf.variable_scope("GnnModel", reuse=reuse):
-            activ = tf.nn.relu
+            self.act_fn = tf.nn.relu
 
-            extracted_features = gnn_net(self.processed_obs, **kwargs)
+            self.gnn_weights, self.gnn_bias = self.gnn_para_init()
+
+            extracted_features = gnn_forward(self.processed_obs, **kwargs)
             extracted_features = tf.layers.flatten(extracted_features)
 
 
-def gnn_net(graph,capacity,failure_prob):
+    def gnn_para_init(self, input_dim, hid_dims, out_dim, dropout, scope):
     
-    return 1
+        weights = []
+        bias = []
+
+        curr_in_dim = E.shape[-1]
+
+        # hidden layers
+        for hid_dim in hid_dims:
+          weights.append(glorot([curr_in_dim, hid_dim], scope=scope))
+          bias.append(zeros([hid_dim], scope=scope))
+          curr_in_dim = hid_dim
+
+        return weights, bias
+
+    def gnn_forward(self, input):
+
+        E = input # edge features matrix [N x N x 2]
+        num_n = E.shape[0]
+        P = E.shape[-1]
+
+        X = tf.ones([num_n, P], tf.float32) # node feature matrix [N x 2]
+
+        y = X 
+        for l in range(len(self.gnn_weights)):
+          y = tf.matmul(y, self.gnn_weights[l])
+          y += self.gnn_bias[l]
+          y = tf.matmul(tf.transpose(E,[2,0,1]), y)
+          y = tf.sigmoid(y)
+
+        return y
+
+
+def glorot(shape, dtype=tf.float32, scope='default'):
+    # Xavier Glorot & Yoshua Bengio (AISTATS 2010) initialization (Eqn 16)
+    with tf.variable_scope(scope):
+        init_range = np.sqrt(6.0 / (shape[0] + shape[1]))
+        init = tf.random_uniform(
+            shape, minval=-init_range, maxval=init_range, dtype=dtype)
+        return tf.Variable(init)
+
+
+def zeros(shape, dtype=tf.float32, scope='default'):
+    with tf.variable_scope(scope):
+        init = tf.zeros(shape, dtype=dtype)
+        return tf.Variable(init)
